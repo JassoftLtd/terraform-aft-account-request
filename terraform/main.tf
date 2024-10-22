34d698ba48d6f1bb34affbfa87f7bc29af53e3b0
jon.shaw <7594012+jonnyshaw89@users.noreply.github.com>
2024-10-22 14:14:11 +0100

    add jon-sandbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-#module "sandbox" {
-#  source = "./modules/aft-account-request"
-#
-#  control_tower_parameters = {
-#    AccountEmail              = "jon.shaw+sandbox@jassoft.co.uk"
-#    AccountName               = "sandbox"
-#    ManagedOrganizationalUnit = "AFT Management"
-#    SSOUserEmail              = "jon.shaw@jassoft.co.uk"
-#    SSOUserFirstName          = "Jon"
-#    SSOUserLastName           = "Shaw"
-#  }
-#
-#  account_tags = {
-#    "Project" = "Test"
-#  }
-#
-#  change_management_parameters = {
-#    change_requested_by = "HashiCorp Learn"
-#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-#  }
-#
-#  custom_fields = {
-#    group = "non-prod"
-#  }
-#
-#  account_customizations_name = "sandbox"
-#}
+module "jon_sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jon.shaw+sandbox@jassoft.co.uk"
+    AccountName               = "jon-sandbox"
+    ManagedOrganizationalUnit = "AFT Management"
+    SSOUserEmail              = "jon.shaw@jassoft.co.uk"
+    SSOUserFirstName          = "Jon"
+    SSOUserLastName           = "Shaw"
+  }
+
+  account_tags = {
+    "Project" = "Test"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
